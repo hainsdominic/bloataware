288f30adcec8324832d8d931d9c8a986f9313d58
Dominic Hains <contact.dominichains@gmail.com>
2022-03-20 15:02:12 -0400

    feat: displays the process using max memory

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,49 @@ use std::{thread, time};
 use sysinfo::{ProcessExt, System, SystemExt};
 
 // goal: identifies the processes that takes too much space in memory and notifies the user
+
+#[derive(Debug)]
+struct MemoyProcess {
+    name: String,
+    pid: String,
+    total_written_bytes: u64,
+}
+
 fn main() {
     let sys = System::new_all();
+    let mut memory_processes = vec![];
+
     for (pid, process) in sys.processes() {
-        println!("[{}] {} {:?}", pid, process.name(), process.disk_usage());
+        let new_process = MemoyProcess {
+            name: process.name().to_string(),
+            pid: pid.to_string(),
+            total_written_bytes: process.disk_usage().total_written_bytes,
+        };
+        memory_processes.push(new_process);
     }
+
     loop {
-        let memory_percentage = sys.used_memory() as f64 / sys.total_memory() as f64;
-        let status = format!("NB processors: {}", memory_percentage);
+        let max = max_memory_processes(&memory_processes);
         Notification::new()
-            .summary("CPU temp")
-            .body(&status)
+            .summary("Bloat alert!")
+            .body(&format!(
+                "The suspect is {} with pid [{}], using {} bytes",
+                max.name, max.pid, max.total_written_bytes
+            ))
             .show()
             .unwrap();
 
         thread::sleep(time::Duration::from_secs(5));
     }
+}
+
+fn max_memory_processes(memory_processes: &Vec<MemoyProcess>) -> &MemoyProcess {
+    let mut max_process = memory_processes.first().unwrap();
+    for process in memory_processes.iter() {
+        if process.total_written_bytes > max_process.total_written_bytes {
+            max_process = process;
+        }
+    }
+
+    return max_process;
 }
